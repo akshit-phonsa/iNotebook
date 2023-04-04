f4b2c6ea6a94aa4370c41d3b18e09b64b4a6b081
akshit-phonsa <phonsaakshit@gmail.com>
2023-04-04 22:22:43 +0530

    first commit

---
+++ backend/db.js
@@ -0,0 +1,7 @@
+const mongoose = require('mongoose');
+const mongoURI = 'mongodb://127.0.0.1:27017/iNotebook'
+const connectToMongo = async () => {
+    await mongoose.connect(mongoURI).then(() => console.log("Connecting TO Mongo Successfully"))
+    .catch(err => console.log(err));
+}
+module.exports = connectToMongo;
